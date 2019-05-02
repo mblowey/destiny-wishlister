562da846c0c01e1e3628955e80475e2d0c2e7e68
mblowey <5872539+mblowey@users.noreply.github.com>
2019-05-02 18:32:16 -0400

    Added wishlist generation. This marks the minimum viable product. Hurray!

--- DestinyWishlister/Services/WishlistGenerator.cs
+++ DestinyWishlister/Services/WishlistGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DestinyWishlisterModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -27,12 +28,75 @@ namespace DestinyWishlister.Services
         }
 
 
-        public async Task<string> Create(WeaponTypeData data)
+        public async Task<string> Create(WeaponTypeData TypeData)
         {
             await Initialization;
 
-            Console.WriteLine("Placeholder Wishlist Creation");
-            return "";
+            string wishlistText = "";
+
+            foreach (var weapon in WeaponData.Weapons)
+            {
+                var subtype = TypeData.Types.Find(t => t.Name == weapon.WeaponType).Subtypes.Find(st => st.Name == weapon.WeaponSubtype);
+
+                var perkHashesBySocket = new List<List<long>>();
+                foreach (var weaponSocket in weapon.Sockets)
+                {
+                    var subtypeSocket = subtype.Sockets.Find(s => s.Name == weaponSocket.Name);
+                    var selectedPerkHashes = GetSelectedPerkHashes(subtypeSocket.Perks);
+
+                    if (selectedPerkHashes.Count == 0)
+                        continue;
+
+                    var matchingPerks = selectedPerkHashes.Where(ph => weaponSocket.Perks.Contains(ph)).ToList();
+                    Console.WriteLine($"{weapon.Name} has {matchingPerks.Count} matching perks in socket {weaponSocket.Name}");
+                    perkHashesBySocket.Add(matchingPerks);
+                }
+
+                List<string> combinations = GetPerkCombinations(perkHashesBySocket);
+
+                foreach (var combo in combinations)
+                {
+                    wishlistText += $"dimwishlist:item={weapon.Hash}&perks={combo}\n";
+                }
+            }
+
+            return wishlistText;
+        }
+
+        private List<long> GetSelectedPerkHashes(List<Perk> perks)
+        {
+            return perks.Where(p => p.IsSelected).Select(p => p.Hash).ToList();
+        }
+
+        private List<string> GetPerkCombinations(List<List<long>> perkHashesBySocket)
+        {
+            var combos = new List<string>();
+            Console.WriteLine($"perkHashesBySocket.Count == {perkHashesBySocket.Count}");
+
+            foreach (var socket in perkHashesBySocket)
+            {
+                var newCombos = new List<string>();
+
+                foreach (var perk in socket)
+                {
+
+                    if (combos.Count == 0)
+                    {
+                        newCombos.Add(perk.ToString());
+                    }
+                    else
+                    {
+                        foreach (var combo in combos)
+                        {
+                            newCombos.Add(combo + "," + perk.ToString());
+                        }
+                    }
+                }
+
+                combos = newCombos;
+            }
+
+            return combos;
         }
     }
 }
